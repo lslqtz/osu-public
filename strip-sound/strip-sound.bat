a94aa41ad717a2d8137eb05e6bb7f679963e2bac
strager <strager.nds@gmail.com>
2010-08-05 18:07:31 -0400

    Compatibility with Explorer bug (not escaping commas and semicolons).

--- strip-sound/strip-sound.bat
+++ strip-sound/strip-sound.bat
@@ -19,12 +19,12 @@ rem   0. You just DO WHAT THE FUCK YOU WANT TO.
 
 rem http://stackoverflow.com/questions/377407/detecting-how-a-batch-file-was-executed
 
-setlocal enableextensions
+setlocal enableextensions enabledelayedexpansion
 
 set SCRIPT=%0
 set DQUOTE="
 
-@echo %SCRIPT:~0,1% | findstr /l %DQUOTE% > NUL
+echo %SCRIPT:~0,1% | findstr /l %DQUOTE% > NUL
 if %ERRORLEVEL% EQU 0 set RUN_FROM_EXPLORER=1
 
 rem Check if any arguments were passed
@@ -41,22 +41,34 @@ if "%~1" == "" (
 
 rem http://stackoverflow.com/questions/3416457/spaces-in-batch-script-arguments
 
-rem Parse each files
-:next
-for %%g in (%*) do (
-	echo %%~g
-	del /Q "%%~dpng-out.avi" 2> NUL
+rem Parse each file
 
-	%~dp0mencoder.exe -msglevel all=3:statusline=0 -nosound -ovc copy -of avi -o "%%~dpng-out.avi" -- "%%~g"
+set "args=%*"
+set "args=%args:,=:comma:%"
+set "args=%args:;=:semicolon:%"
+
+for %%g in (%args%) do (
+	set "infile=%%~g"
+	set "infile=!infile::comma:=,!"
+	set "infile=!infile::semicolon:=;!"
+
+	set "outfile=%%~dpng-out.avi"
+	set "outfile=!outfile::comma:=,!"
+	set "outfile=!outfile::semicolon:=;!"
+
+	echo !infile!
+	del /Q "!outfile!" 2> NUL
+
+	%~dp0mencoder.exe -endpos 60 -msglevel "all=3:statusline=0" -nosound -ovc copy -of avi -o "!outfile!" -- "!infile!"
 
 	rem For some reason, mencoder doesn't set the error level.
 	rem To detect if the encode worked or not, we check if the
 	rem output file exists.  If a file is reencoded, the old
 	rem output file may still exist, so we need to delete it.
 
-	if not exist "%%~dpng-out.avi" (
+	if not exist !outfile! (
 		echo.
-		echo Encoding %%~g failed; see above >&2
+		echo Encoding !infile! failed; see above >&2
 		call :end
 	)
 )
